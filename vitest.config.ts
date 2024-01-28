eeb22fe2d0e50cbf3d7ca491e63eaafdf7c47fad
heathernfran <heathernfran@gmail.com>
2024-01-28 08:50:30 -0800

    Resolve import paths using vite-tsconfig-paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   test: {
     environment: "jsdom",
   },
